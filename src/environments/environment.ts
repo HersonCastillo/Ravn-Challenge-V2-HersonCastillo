412966e0a38b16ed4b97f906bbcfde58d999389a
Herson Castillo <hersoncastillo12@hotmail.com>
2021-10-15 23:05:03 -0600

    Creating develop branch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  GRAPHQL_ENDPOINT:
+    'https://swapi-graphql.netlify.app/.netlify/functions/index',
 };
 
 /*
